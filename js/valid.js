1742554ab03201d23a0ab8061990543b4172c4cc
AbelSantJim <151285328+AbelSantJim@users.noreply.github.com>
2025-07-08 06:50:38 -0600

    Update valid.js

--- js/valid.js
+++ js/valid.js
@@ -15,3 +15,11 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     mensaje.textContent = "Correo o contraseña incorrectos.";
   }
 });
+document.getElementById("olvidoContrasena").addEventListener("click", function(){
+  alert("Se ha mandado un codigo de verificacion a su correo de respaldo");
+});
+
+document.getElementById("registrate").addEventListener("click", function(){
+  alert("En un momento se abrira una nueva pestaña");
+})
+
