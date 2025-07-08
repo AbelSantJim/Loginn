764a8d46433144b3a3398082ea8fc1020dc62ab0
AbelSantJim <151285328+AbelSantJim@users.noreply.github.com>
2025-07-08 09:40:58 -0600

    Update valid.js

--- js/valid.js
+++ js/valid.js
@@ -6,20 +6,22 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   if (correo === "abel@gmail.com" && contrasena === "12345678") {
     mensaje.style.color = "green";
     mensaje.textContent = "Inicio de sesión exitoso. Redirigiendo...";
-
-    setTimeout(() => {
-      window.open("https://abelsantjim.github.io/PortafolioAbel/", "_blank");
-    }, 1000);
+    window.open("https://abelsantjim.github.io/PortafolioAbel/", "_blank");
   } else {
     mensaje.style.color = "red";
     mensaje.textContent = "Correo o contraseña incorrectos.";
   }
 });
 document.getElementById("olvidoContrasena").addEventListener("click", function(){
-  alert("Se ha mandado un codigo de verificacion a su correo de respaldo");
+window.open("http://localhost/login/olvido.html", "_blank");
 });
 
 document.getElementById("registrate").addEventListener("click", function(){
   alert("En un momento se abrira una nueva pestaña");
+  window.open("http://localhost/login/registro.html", "_blank");
+ 
+});
+document.getElementById("btnRecuperar").addEventListener("click", function(){
+  alert("Se ha mandado un codigo de verificacion a su correo de respaldo");
 });
 
