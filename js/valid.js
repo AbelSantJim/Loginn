00899563986e7d06fe59c84d18fa7e663c00bc5c
AbelSantJim <151285328+AbelSantJim@users.noreply.github.com>
2025-07-09 07:48:00 -0600

    Update valid.js

--- js/valid.js
+++ js/valid.js
@@ -2,7 +2,6 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   const correo = document.getElementById("correo").value.trim();
   const contrasena = document.getElementById("contrasena").value.trim();
   const mensaje = document.getElementById("mensaje");
-
   if (correo === "abel@gmail.com" && contrasena === "12345678") {
     mensaje.style.color = "green";
     mensaje.textContent = "Inicio de sesión exitoso. Redirigiendo...";
@@ -20,7 +19,3 @@ document.getElementById("registrate").addEventListener("click", function(){
   alert("En un momento se abrira una nueva pestaña");
   window.open("registro.html", "_blank"); 
 });
-document.getElementById("btnRecuperar").addEventListener("click", function(){
-  alert("Se ha mandado un codigo de verificacion a su correo de respaldo");
-});
-
